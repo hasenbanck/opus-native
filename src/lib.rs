957918cf08411adc249d694f3b56be05ac4cba9d
Nils Hasenbanck <nils@hasenbanck.de>
2021-04-27 16:46:21 +0200

    Add the general project layout.

---
+++ src/lib.rs
@@ -0,0 +1,7 @@
+#![warn(missing_docs)]
+#![deny(unsafe_code)]
+#![deny(unused_results)]
+#![deny(clippy::as_conversions)]
+#![deny(clippy::panic)]
+#![deny(clippy::unwrap_used)]
+//! A Rust native implementation of the audio codec Opus.
