f8fae11eae757cab909fcc9c772e2ad1caa1abe3
Nils Hasenbanck <nils@hasenbanck.de>
2021-05-26 19:49:22 +0200

    Add optional FMA support for the comb_filter().

--- src/celt/comb_filter/avx.rs
+++ src/celt/comb_filter/avx.rs
@@ -25,7 +25,7 @@ pub(crate) fn comb_filter_const(
 
         let mut j = 0;
         (0..n - 7).into_iter().step_by(8).for_each(|i| {
-            let yi = _mm256_loadu_ps(x[x_offset + i..].as_ptr());
+            let mut yi = _mm256_loadu_ps(x[x_offset + i..].as_ptr());
 
             let x0v = _mm256_loadu_ps(x[x_offset + i - t - 2..].as_ptr());
             let x4v = _mm256_loadu_ps(x[x_offset + i - t + 2..].as_ptr());
@@ -34,9 +34,18 @@ pub(crate) fn comb_filter_const(
             let x1v = _mm256_shuffle_ps(x0v, x2v, 0b10_01_10_01);
             let x3v = _mm256_shuffle_ps(x2v, x4v, 0b10_01_10_01);
 
-            let yi = _mm256_add_ps(yi, _mm256_mul_ps(g10v, x2v));
-            let yi = _mm256_add_ps(yi, _mm256_mul_ps(g11v, _mm256_add_ps(x3v, x1v)));
-            let yi = _mm256_add_ps(yi, _mm256_mul_ps(g12v, _mm256_add_ps(x4v, x0v)));
+            #[cfg(target_feature = "fma")]
+            {
+                yi = _mm256_fmadd_ps(g10v, x2v, yi);
+                yi = _mm256_fmadd_ps(g11v, _mm256_add_ps(x3v, x1v), yi);
+                yi = _mm256_fmadd_ps(g12v, _mm256_add_ps(x4v, x0v), yi);
+            }
+            #[cfg(not(target_feature = "fma"))]
+            {
+                yi = _mm256_add_ps(yi, _mm256_mul_ps(g10v, x2v));
+                yi = _mm256_add_ps(yi, _mm256_mul_ps(g11v, _mm256_add_ps(x3v, x1v)));
+                yi = _mm256_add_ps(yi, _mm256_mul_ps(g12v, _mm256_add_ps(x4v, x0v)));
+            }
 
             _mm256_storeu_ps(y[y_offset + i..].as_mut_ptr(), yi);
 
@@ -50,16 +59,25 @@ pub(crate) fn comb_filter_const(
         let mut x0v = _mm_loadu_ps(x[x_offset + j - t - 2..].as_ptr());
 
         if (n - j) != 0 {
-            let yi = _mm_loadu_ps(x[x_offset + j..].as_ptr());
+            let mut yi = _mm_loadu_ps(x[x_offset + j..].as_ptr());
             let x4v = _mm_loadu_ps(x[x_offset + j - t + 2..].as_ptr());
 
             let x2v = _mm_shuffle_ps(x0v, x4v, 0b01_00_11_10);
             let x1v = _mm_shuffle_ps(x0v, x2v, 0b10_01_10_01);
             let x3v = _mm_shuffle_ps(x2v, x4v, 0b10_01_10_01);
 
-            let yi = _mm_add_ps(yi, _mm_mul_ps(g10v, x2v));
-            let yi = _mm_add_ps(yi, _mm_mul_ps(g11v, _mm_add_ps(x3v, x1v)));
-            let yi = _mm_add_ps(yi, _mm_mul_ps(g12v, _mm_add_ps(x4v, x0v)));
+            #[cfg(target_feature = "fma")]
+            {
+                yi = _mm_fmadd_ps(g10v, x2v, yi);
+                yi = _mm_fmadd_ps(g11v, _mm_add_ps(x3v, x1v), yi);
+                yi = _mm_fmadd_ps(g12v, _mm_add_ps(x4v, x0v), yi);
+            }
+            #[cfg(not(target_feature = "fma"))]
+            {
+                yi = _mm_add_ps(yi, _mm_mul_ps(g10v, x2v));
+                yi = _mm_add_ps(yi, _mm_mul_ps(g11v, _mm_add_ps(x3v, x1v)));
+                yi = _mm_add_ps(yi, _mm_mul_ps(g12v, _mm_add_ps(x4v, x0v)));
+            }
 
             x0v = x4v;
             _mm_storeu_ps(y[y_offset + j..].as_mut_ptr(), yi);
@@ -87,7 +105,7 @@ pub(crate) fn comb_filter_const_inplace(
 
         let mut j = 0;
         (0..n - 7).into_iter().step_by(8).for_each(|i| {
-            let yi = _mm256_loadu_ps(y[y_offset + i..].as_ptr());
+            let mut yi = _mm256_loadu_ps(y[y_offset + i..].as_ptr());
 
             let x0v = _mm256_loadu_ps(y[y_offset + i - t - 2..].as_ptr());
             let x4v = _mm256_loadu_ps(y[y_offset + i - t + 2..].as_ptr());
@@ -96,9 +114,18 @@ pub(crate) fn comb_filter_const_inplace(
             let x1v = _mm256_shuffle_ps(x0v, x2v, 0b10_01_10_01);
             let x3v = _mm256_shuffle_ps(x2v, x4v, 0b10_01_10_01);
 
-            let yi = _mm256_add_ps(yi, _mm256_mul_ps(g10v, x2v));
-            let yi = _mm256_add_ps(yi, _mm256_mul_ps(g11v, _mm256_add_ps(x3v, x1v)));
-            let yi = _mm256_add_ps(yi, _mm256_mul_ps(g12v, _mm256_add_ps(x4v, x0v)));
+            #[cfg(target_feature = "fma")]
+            {
+                yi = _mm256_fmadd_ps(g10v, x2v, yi);
+                yi = _mm256_fmadd_ps(g11v, _mm256_add_ps(x3v, x1v), yi);
+                yi = _mm256_fmadd_ps(g12v, _mm256_add_ps(x4v, x0v), yi);
+            }
+            #[cfg(not(target_feature = "fma"))]
+            {
+                yi = _mm256_add_ps(yi, _mm256_mul_ps(g10v, x2v));
+                yi = _mm256_add_ps(yi, _mm256_mul_ps(g11v, _mm256_add_ps(x3v, x1v)));
+                yi = _mm256_add_ps(yi, _mm256_mul_ps(g12v, _mm256_add_ps(x4v, x0v)));
+            }
 
             _mm256_storeu_ps(y[y_offset + i..].as_mut_ptr(), yi);
 
@@ -112,16 +139,25 @@ pub(crate) fn comb_filter_const_inplace(
         let mut x0v = _mm_loadu_ps(y[y_offset + j - t - 2..].as_ptr());
 
         if (n - j) != 0 {
-            let yi = _mm_loadu_ps(y[y_offset + j..].as_ptr());
+            let mut yi = _mm_loadu_ps(y[y_offset + j..].as_ptr());
             let x4v = _mm_loadu_ps(y[y_offset + j - t + 2..].as_ptr());
 
             let x2v = _mm_shuffle_ps(x0v, x4v, 0b01_00_11_10);
             let x1v = _mm_shuffle_ps(x0v, x2v, 0b10_01_10_01);
             let x3v = _mm_shuffle_ps(x2v, x4v, 0b10_01_10_01);
 
-            let yi = _mm_add_ps(yi, _mm_mul_ps(g10v, x2v));
-            let yi = _mm_add_ps(yi, _mm_mul_ps(g11v, _mm_add_ps(x3v, x1v)));
-            let yi = _mm_add_ps(yi, _mm_mul_ps(g12v, _mm_add_ps(x4v, x0v)));
+            #[cfg(target_feature = "fma")]
+            {
+                yi = _mm_fmadd_ps(g10v, x2v, yi);
+                yi = _mm_fmadd_ps(g11v, _mm_add_ps(x3v, x1v), yi);
+                yi = _mm_fmadd_ps(g12v, _mm_add_ps(x4v, x0v), yi);
+            }
+            #[cfg(not(target_feature = "fma"))]
+            {
+                yi = _mm_add_ps(yi, _mm_mul_ps(g10v, x2v));
+                yi = _mm_add_ps(yi, _mm_mul_ps(g11v, _mm_add_ps(x3v, x1v)));
+                yi = _mm_add_ps(yi, _mm_mul_ps(g12v, _mm_add_ps(x4v, x0v)));
+            }
 
             x0v = x4v;
             _mm_storeu_ps(y[y_offset + j..].as_mut_ptr(), yi);
